6908b19d406c00b3f38d132b094b5cb41e9ec7a8
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-20 06:02:36 -0800

    Selection sort

--- sort.h
+++ sort.h
@@ -37,6 +37,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
 
 
 
